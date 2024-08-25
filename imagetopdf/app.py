f8d2eb9d555f0ce5303ae0bea6e20a8954e154ce
Gloria Chepkoech <gloriachepkoech223@gmail.com>
2024-08-25 16:58:08 +0300

    commits

--- imagetopdf/app.py
+++ imagetopdf/app.py
@@ -15,6 +15,26 @@ class ImageToPDFConverter:
     title_label = tk.Label(self.root, text="Image to PDF Converter", font=("Helvetica",16,"bold"))
     title_label.pack(pady=10)
 
+    select_images_button = tk.Button(self.root,text="select Images", command=self.select_images)
+    select_images_button.pack(pady=(0, 10)) 
+
+    self.selected_images_listbox.pack(pady=(0,10), fill=tk.BOTH, expand=True)
+
+    label = tk.label (self.root, text="Enter output PDF name:")
+    label.pack()
+
+    pdf_name_entry = tk.Entry(self.root, textvariable=self.output_pdf_name, widht=40,
+    justify='center')
+    pdf_name_entry.pack()
+
+    convert_button = tk.Button(self.root, text="convert to PDF", command=self.
+    convert_images_to_pdf)
+    convert_button.pack(pady=(20, 40))
+
+    def select_images(self):
+      self.image_paths = filedialog.askopenfilenames(title="Select Images", filetypes=
+      [("Image files","*.png;*.jpg;*jpeg")])
+      self.update_selected_images_listbox()
 def main():
   root = tk.Tk()
   root.title("Image to PDF")
